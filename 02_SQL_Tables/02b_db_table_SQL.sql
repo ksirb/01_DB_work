a3282df3167982cf5f91b1f18c7ebf3e911ec259
ksirb <mia.kamenetski@online.de>
2022-12-06 11:58:32 +0000

    02b_db_table_SQL.sql

--- 02_SQL_Tables/02b_db_table_SQL.sql
+++ 02_SQL_Tables/02b_db_table_SQL.sql
@@ -1,9 +1,6 @@
 /* ------- Structuren UNIQUE ----------- */
 
-/*  Doppelte DS werden nicht mehr zugelassen
-    UNIQUE
-*/
-
+/* Auto Zahlen */
 
 DROP DATABASE IF EXISTS boo;
 
@@ -11,13 +8,11 @@ CREATE DATABASE IF NOT EXISTS boo;
 
 CREATE TABLE IF NOT EXISTS boo.test
 (
-    name VARCHAR(20) NOT NULL UNIQUE DEFAULT "TBA",
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(20) NOT NULL DEFAULT "TBA",
     age INT(3) NOT NULL DEFAULT 0
 );
 
-
-/* Alle Tabellen in der DB anzeigen */
-# SHOW TABLES; - with USE boo
 
 DESCRIBE boo.test;
 
